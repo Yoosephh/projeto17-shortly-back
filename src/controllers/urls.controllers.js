54e86fc5ea5d1a9cdee47c809ffc791ae36ea9f6
José Eduardo <105430849+Yoosephh@users.noreply.github.com>
2023-08-03 23:59:28 -0300

    feat: redirect route

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -11,7 +11,7 @@ export async function createUrl(req,res) {
 
     const shortUrl = nanoid(8)
 
-    const obj = await db.query(`INSERT INTO urls ("URLs", "shortenedURL", "userId") VALUES ($1, $2, $3) RETURNING id`, [url, shortUrl, checkUser.rows[0].userId])
+    const obj = await db.query(`INSERT INTO urls ("url", "shortUrl", "userId") VALUES ($1, $2, $3) RETURNING id`, [url, shortUrl, checkUser.rows[0].userId])
 
     res.status(201).send({id: obj.rows[0].id, shortUrl})
   }catch (err){
@@ -25,7 +25,7 @@ export async function sendUrl(req,res) {
     const checkUrl = await db.query(`SELECT * FROM "urls" where id = $1`, [urlId])
     if(checkUrl.rowCount === 0) return res.sendStatus(404)
 
-    const { id, shortenedURL: shortUrl, URLs:url} = checkUrl.rows[0]
+    const { id, shortUrl, url} = checkUrl.rows[0]
     console.log(checkUrl.rows[0])
     res.status(200).send({id, shortUrl, url})
 
@@ -35,8 +35,13 @@ export async function sendUrl(req,res) {
 }
 
 export async function redirectUser(req,res) {
+  const {shortUrl} = req.params
   try{
+    const checkUrl = await db.query(`SELECT * FROM urls WHERE "shortUrl" = $1`, [shortUrl])
+    if(checkUrl.rowCount === 0) return res.sendStatus(404)
 
+    await db.query(`UPDATE urls SET "views" = "views" + 1 WHERE "shortUrl" = $1`, [shortUrl])
+    res.redirect(checkUrl.rows[0].url)
   }catch (err){
     console.log(err)
   }
